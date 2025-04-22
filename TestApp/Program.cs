d3255256260bd0fb31233e2c7f6f690f68075730
Steen F. Tøttrup <sft@creativeminds.dk>
2025-04-22 21:09:31 +0200

    again

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
 Console.WriteLine("Hello, World!");
 CancellationToken cancellationToken = new CancellationToken();
 
